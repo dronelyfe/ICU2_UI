c646fc16a78ec559c51b16bccddc7b3960085d3c
Greg Wilson <33581888+dronelyfe@users.noreply.github.com>
2019-10-04 15:17:33 -0400

    Add files via upload

--- plots.R
+++ plots.R
@@ -3,5 +3,27 @@ flowers <- iris
 
 ## ex. mockup for now; We just want to try out some geometries and such
 ## then, we can be ready to just slot our production plots right in.
-carplot <- ggplot(data = cars, aes(x = model, y = displ)) + geom_point()
+carplot <- ggplot(
+    data = cars, 
+    aes(x = model, y = displ)) + 
+    geom_point() +
+    theme(
+        axis.text.x = element_blank()
+    )
 
+## plots for primary tab, 'plot' to be replaced with metric or variable name when ready
+## fill out with aesthetic mappings and geometries
+primary_plot_1 <- ggplot() 
+primary_plot_2 <- ggplot()
+primary_plot_3 <- ggplot()
+primary_plot_4 <- ggplot()
+primary_plot_5 <- ggplot()
+primary_plot_6 <- ggplot()
+
+## plots for secondary tab, 'plot' to be replaced with metric or variable name when ready
+secondary_plot_1 <- ggplot()
+secondary_plot_2 <- ggplot()
+secondary_plot_3 <- ggplot()
+secondary_plot_4 <- ggplot()
+secondary_plot_5 <- ggplot()
+secondary_plot_6 <- ggplot()
